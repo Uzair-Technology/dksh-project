779d82dc5cfa5e347ca2fda1bd60439e1f331f04
Dex Coder <60599437+mhdkashif07@users.noreply.github.com>
2023-04-05 00:16:35 +0500

    review modal

--- src/components/cards/GridCard.jsx
+++ src/components/cards/GridCard.jsx
@@ -2,12 +2,7 @@ import React from "react";
 
 import { GoComment } from "react-icons/go";
 
-import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
