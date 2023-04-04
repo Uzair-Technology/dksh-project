393be3280e3b308798344f72e8a4f7dd7ab89a43
Dex Coder <60599437+mhdkashif07@users.noreply.github.com>
2023-04-05 02:33:08 +0500

    price menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,6 +28,124 @@ const Buttons = [
   },
 ];
 
+import { styled, alpha, Box } from "@mui/system";
+import SliderUnstyled, {
+  sliderUnstyledClasses,
+} from "@mui/base/SliderUnstyled";
+
+const blue = {
+  100: "#DAECFF",
+  200: "#99CCF3",
+  400: "#3399FF",
+  300: "#66B2FF",
+  500: "#007FFF",
+  600: "#0072E5",
+  900: "#003A75",
+};
+
+const grey = {
+  50: "#f6f8fa",
+  100: "#eaeef2",
+  200: "#d0d7de",
+  300: "#afb8c1",
+  400: "#8c959f",
+  500: "#6e7781",
+  600: "#57606a",
+  700: "#424a53",
+  800: "#32383f",
+  900: "#24292f",
+};
+
+const StyledSlider = styled(SliderUnstyled)(
+  ({ theme }) => `
+  color: ${theme.palette.mode === "light" ? blue[500] : blue[300]};
+  height: 6px;
+  width: 100%;
+  padding: 16px 0;
+  display: inline-block;
+  position: relative;
+  cursor: pointer;
+  touch-action: none;
+  -webkit-tap-highlight-color: transparent;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  &.${sliderUnstyledClasses.disabled} { 
+    pointer-events: none;
+    cursor: default;
+    color: ${theme.palette.mode === "light" ? grey[300] : grey[600]};
+    opacity: 0.5;
+  }
+
+  & .${sliderUnstyledClasses.rail} {
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 4px;
+    border-radius: 2px;
+    background-color: currentColor;
+    opacity: 0.4;
+  }
+
+  & .${sliderUnstyledClasses.track} {
+    display: block;
+    position: absolute;
+    height: 4px;
+    border-radius: 2px;
+    background-color: currentColor;
+  }
+
+  & .${sliderUnstyledClasses.thumb} {
+    position: absolute;
+    width: 16px;
+    height: 16px;
+    margin-left: -6px;
+    margin-top: -6px;
+    box-sizing: border-box;
+    border-radius: 50%;
+    outline: 0;
+    border: 3px solid currentColor;
+    background-color: #fff;
+
+    :hover,
+    &.${sliderUnstyledClasses.focusVisible} {
+      box-shadow: 0 0 0 0.25rem ${alpha(
+        theme.palette.mode === "light" ? blue[400] : blue[300],
+        0.15
+      )};
+    }
+
+    &.${sliderUnstyledClasses.active} {
+      box-shadow: 0 0 0 0.25rem ${alpha(
+        theme.palette.mode === "light" ? blue[200] : blue[300],
+        0.3
+      )};
+    }
+  }
+
+  & .${sliderUnstyledClasses.mark} {
+    position: absolute;
+    width: 4px;
+    height: 4px;
+    border-radius: 2px;
+    background-color: currentColor;
+    top: 50%;
+    opacity: 0.7;
+    transform: translateX(-50%);
+  }
+
+  & .${sliderUnstyledClasses.markActive} {
+    background-color: #fff;
+  }
+`
+);
+
+function valuetext(value) {
+  return `${value}°C`;
+}
+
 const Home = () => {
   //*states
   const [data, setData] = useState(jsonData);
@@ -38,6 +156,13 @@ const Home = () => {
   const [service, setService] = useState("all");
   const [price, setPrice] = useState("");
   const [sort, setSort] = useState("");
+
+  const [value, setValue] = React.useState([300, 700]);
+  console.log(value);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   console.log(available, talent, service, price, sort);
 
@@ -50,6 +175,16 @@ const Home = () => {
   };
 
   //* handle change for price
+  const [anchorEl1, setAnchorEl1] = React.useState(null);
+  const open1 = Boolean(anchorEl1);
+  const handleClick1 = (event) => {
+    setAnchorEl1(event.currentTarget);
+  };
+  const handleClose1 = () => {
+    setAnchorEl1(null);
+  };
+
+  //**for second dropdown */
   const [anchorEl2, setAnchorEl2] = React.useState(null);
   const open2 = Boolean(anchorEl2);
   const handleClick2 = (event) => {
@@ -59,6 +194,7 @@ const Home = () => {
     setAnchorEl2(null);
   };
 
+  //**for third dropdown */
   const [anchorEl3, setAnchorEl3] = React.useState(null);
   const open3 = Boolean(anchorEl3);
   const handleClick3 = (event) => {
@@ -152,31 +288,70 @@ const Home = () => {
               <div className="search__section--right">
                 <div className="search__section--price">
                   <div className="search__section--dropdown">
-                    <label htmlFor="prices">
+                    <label
+                      htmlFor="prices"
+                      id="basic-button"
+                      aria-controls={open1 ? "basic-menu" : undefined}
+                      aria-haspopup="true"
+                      aria-expanded={open1 ? "true" : undefined}
+                      onClick={handleClick1}
+                    >
                       <FaMoneyBillWave /> Price per hr:
                     </label>
-                    {/* <button
-                      id="basic-button"
-                      aria-controls={open ? "basic-menu" : undefined}
-                      aria-haspopup="true"
-                      aria-expanded={open ? "true" : undefined}
-                      onClick={handleClick}
-                    >
-                      All
-                    </button>
                     <Menu
                       id="basic-menu"
-                      anchorEl={anchorEl}
-                      open={open}
-                      onClose={handleClose}
+                      anchorEl={anchorEl1}
+                      open={open1}
+                      onClose={handleClose1}
                       MenuListProps={{
                         "aria-labelledby": "basic-button",
                       }}
+                      PaperProps={{
+                        style: {
+                          maxHeight: ITEM_HEIGHT * 50,
+                          width: "30ch",
+                          marginTop: "1ch",
+                          marginLeft: "-.7ch",
+                        },
+                      }}
                     >
-                      <MenuItem onClick={handleClose}>Profile</MenuItem>
-                      <MenuItem onClick={handleClose}>My account</MenuItem>
-                      <MenuItem onClick={handleClose}>Logout</MenuItem>
-                    </Menu> */}
+                      <div className="search__section--slider">
+                        <p>Price</p>
+                        <div className="search__section--range">
+                          {/* controlled: */}
+                          <StyledSlider
+                            value={value}
+                            onChange={handleChange}
+                            getAriaLabel={() => "Temperature range"}
+                            getAriaValueText={valuetext}
+                            min={0}
+                            max={1000}
+                          />
+                        </div>
+                        <div className="search__section--prices flex-between">
+                          <span>0$</span>
+                          <span>1000$</span>
+                        </div>
+                        <div className="search__section--input">
+                          <div className="search__section--from">
+                            <label htmlFor="from">From</label>
+                            <input type="text" placeholder="100$" />
+                          </div>
+                          <div className="search__section--to">
+                            <label htmlFor="from">To</label>
+                            <input type="text" placeholder="800$" />
+                          </div>
+                        </div>
+                        <div className="search__section--buttons">
+                          <div className="search__section--cancel">
+                            <button onClick={handleClose1}>Cancel</button>
+                          </div>
+                          <div className="search__section--apply">
+                            <button>Apply</button>
+                          </div>
+                        </div>
+                      </div>
+                    </Menu>
                   </div>
                 </div>
                 <div className="search__section--services">
