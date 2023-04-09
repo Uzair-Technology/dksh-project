539a4be370626af50f4ff8cb449444f0d6bd1c72
Dex Coder <60599437+mhdkashif07@users.noreply.github.com>
2023-04-10 02:34:40 +0500

    added hamburger

--- src/common/navbar/Navbar.jsx
+++ src/common/navbar/Navbar.jsx
@@ -5,11 +5,18 @@ import { BiHomeCircle, BiShoppingBag } from "react-icons/bi";
 import { IoPeopleOutline } from "react-icons/io5";
 import { MdNotifications, MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { BsChatDots } from "react-icons/bs";
+import { AnimatePresence, motion, useCycle } from "framer-motion";
 
 import { Link } from "react-router-dom";
 import { CgFormatSlash } from "react-icons/cg";
 import List from "../../components/list/List";
 import jsonData from "../../assets/data/data.json";
+
+import {
+  GiFamilyHouse,
+  GiHamburgerMenu,
+  GiTireIronCross,
+} from "react-icons/gi";
 
 const setDark = () => {
   localStorage.setItem("theme", "dark");
@@ -44,7 +51,32 @@ const toggleTheme = (e) => {
   }
 };
 
+const itemVariants = {
+  closed: {
+    opacity: 0,
+  },
+  open: {
+    opacity: 1,
+  },
+};
+
+const sideVariants = {
+  closed: {
+    transition: {
+      staggerChildren: 0.2,
+      staggerDirection: -1,
+    },
+  },
+  open: {
+    transition: {
+      staggerChildren: 0.2,
+      staggerDirection: 1,
+    },
+  },
+};
+
 const Navbar = () => {
+  const [open, cycleOpen] = useCycle(false, true);
   const [inputText, setInputText] = useState("");
   const [filteredList, setFilteredList] = useState([]);
   const [showMenu, setShowMenu] = useState(false);
@@ -117,7 +149,7 @@ const Navbar = () => {
               )}
             </div>
           </Grid>
-          <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+          <Grid item xs={0} sm={8} md={4} lg={4} xl={4}>
             <div className="navbar__container--search">
               <input
                 placeholder="Search here"
@@ -142,7 +174,154 @@ const Navbar = () => {
               )}
             </div>
           </Grid>
-          <Grid item xs={3} sm={4} md={4} lg={4} xl={4}>
+
+          {/* animated hamburger */}
+          <Grid item xs={7} sm={7} md={4} lg={4} xl={4}>
+            <div>
+              <div className="navbar__container--hamburger">
+                <AnimatePresence>
+                  {open && (
+                    <motion.aside
+                      initial={{ translateX: 300 }}
+                      animate={{
+                        width: "100%",
+                        //x: "-300px"
+                        translateX: 0,
+                      }}
+                      exit={{
+                        //width: 0,
+                        translateX: 300,
+                        transition: { delay: 0.7, duration: 0.3 },
+                      }}
+                    >
+                      <motion.div
+                        className="hamburger__container"
+                        initial="closed"
+                        animate="open"
+                        exit="closed"
+                        variants={sideVariants}
+                      >
+                        <div>
+                          <ul>
+                            <motion.li
+                              whileHover={{ scale: 1.1 }}
+                              variants={itemVariants}
+                            >
+                              <BiHomeCircle />
+                              <Link hrefLang="/"> Home</Link>
+                            </motion.li>
+                            <motion.li>
+                              <IoPeopleOutline />
+                              <Link hrefLang="/">Network</Link>
+                            </motion.li>
+                            <motion.li>
+                              <BiShoppingBag />
+                              <Link hrefLang="/"> Jobs</Link>
+                            </motion.li>
+                            <div className="toggle-theme-wrapper">
+                              <label
+                                className="toggle-theme"
+                                htmlFor="checkbox"
+                              >
+                                <input
+                                  type="checkbox"
+                                  id="checkbox"
+                                  onChange={toggleTheme}
+                                  defaultChecked={defaultDark}
+                                />
+                                <div className="slider round"></div>
+                              </label>
+                            </div>
+                            <div
+                              className="navbar__container--info"
+                              style={{ display: "inline" }}
+                            >
+                              <div className="navbar__container--info1">
+                                <ul>
+                                  <li>
+                                    <Link hrefLang="/">
+                                      {" "}
+                                      <BsChatDots />
+                                      <div className="navbar__container--online">
+                                        <div></div>
+                                      </div>
+                                    </Link>
+                                  </li>
+                                  <li>
+                                    <Link hrefLang="/">
+                                      {" "}
+                                      <MdNotifications size={23} />
+                                      <div className="navbar__container--online">
+                                        <div></div>
+                                      </div>
+                                    </Link>
+                                  </li>
+                                  <li style={{ marginTop: "-10px" }}>
+                                    <span> John</span>
+                                    <Avatar
+                                      alt="Cindy Baker"
+                                      xs={{ innerHeight: 18 }}
+                                      src={`/images/male-02.png`}
+                                    />
+                                    <MdOutlineKeyboardArrowDown />
+                                  </li>
+                                </ul>
+                              </div>
+                            </div>
+
+                            {/* <Link href="/">
+                                <a href="">
+                                  <SearchOutlined />
+                                </a>
+                              </Link> */}
+
+                            {/* {useAuth.isUserAuthenticated() ? (
+                              <motion.li
+                                whileHover={{ scale: 1.1 }}
+                                variants={itemVariants}
+                                className="cart__logo"
+                              >
+                                <Link href="/cart">
+                                  <a href="">
+                                    <MdShoppingCart />
+                                  </a>
+                                </Link>
+                                <span className="total__items">
+                                  {cartTotalQuantity}
+                                </span>
+                              </motion.li>
+                            ) : (
+                              ''
+                            )} */}
+
+                            <motion.li
+                              whileHover={{ scale: 1.1 }}
+                              variants={itemVariants}
+                            >
+                              {/* <Link href="/login">
+                                <a href="">
+                                  <BsFillPersonFill />
+                                </a>
+                              </Link> */}
+                            </motion.li>
+                          </ul>
+                        </div>
+                      </motion.div>
+                    </motion.aside>
+                  )}
+                </AnimatePresence>
+                <button onClick={() => cycleOpen()}>
+                  {open ? (
+                    <GiTireIronCross size={22} />
+                  ) : (
+                    <GiHamburgerMenu size={25} />
+                  )}
+                </button>
+              </div>
+            </div>
+          </Grid>
+
+          <Grid item xs={0} sm={0} md={4} lg={4} xl={4}>
             <div className="navbar__container--options">
               <div className="navbar__container--links">
                 <ul>
@@ -173,7 +352,7 @@ const Navbar = () => {
               </div>
             </div>
           </Grid>
-          <Grid item xs={2.3} sm={2.3} md={2.3} lg={2.3} xl={2.3}>
+          <Grid item xs={0} sm={0} md={2.3} lg={2.3} xl={2.3}>
             <div className="navbar__container--info">
               <div className="navbar__container--info1">
                 <ul>
